dd28dac8429860cf142ecd5ca139a937b8aafdb7
mrysan <mrysann@gmail.com>
2025-07-22 14:40:00 -0700

    create ToDoForm + ToDoList componenets and use them in App.jsx

---
+++ src/ToDoForm.jsx
@@ -0,0 +1,11 @@
+function ToDoForm() {
+  return (
+    <>
+      <label htmlFor="toDoTitle">toDo</label>
+      <input type="text" id="toDoTitlte"></input>
+      <button>Add ToDo</button>
+    </>
+  );
+}
+
+export default ToDoForm;
